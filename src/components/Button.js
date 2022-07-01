7abf8b2d41da2f3aee083cdf10c28b046c92ef8e
Muhammad Labib <99881858+labibofficial21@users.noreply.github.com>
2022-07-01 19:28:28 +0700

    Delete src/components directory

--- src/components/Button.js
+++
@@ -1,9 +0,0 @@
-const Button = ({ className, onClick, type, children }) => {
-  return (
-    <button className={`btn ${className}`} onClick={onClick} type={type}>
-      {children}
-    </button>
-  );
-};
-
-export default Button;
